c521550c2c81b1271e6b785c18fb5298d3c73b4e
leighd2008 <leighd2008@gmail.com>
2019-05-30 08:59:41 -0400

    added update profile functionality and seed data

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;  
 
-INSERT into users (name, email, entries, joined ) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
-INSERT into login (hash, email) values ('$2a$10$WVG6NyU4PjED.2vIEpbOj.tFZohd8ZM/0QLLrujtIOUIdYwIWcyI.', 'jessie@gmail.com');
+INSERT into users (name, email, entries, joined ) values ('a', 'a@a.com', 5, '2018-01-01');
+INSERT into login (hash, email) values ('$2a$10$WVG6NyU4PjED.2vIEpbOj.tFZohd8ZM/0QLLrujtIOUIdYwIWcyI.', 'a@a.com');
 
 COMMIT;
